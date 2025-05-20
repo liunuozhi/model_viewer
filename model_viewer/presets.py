983898c3bd7100035e7765e43a948344c9dfba77
liu <liunuozhi1995@gmail.com>
2025-05-20 16:06:11 +0800

    Reformat; Remove unused files

--- model_viewer/presets.py
+++ model_viewer/presets.py
@@ -88,6 +88,7 @@ class CameraPreset:
 
         return camera
 
+
 class FollowCameraPreset:
     def add_follow_camera(
         self,
--- model_viewer/utils.py
+++ model_viewer/utils.py
@@ -13,23 +13,23 @@ def scale_to_unit_cube(obj):
     max_y = max(corner.y for corner in bbox_corners)
     min_z = min(corner.z for corner in bbox_corners)
     max_z = max(corner.z for corner in bbox_corners)
-    
+
     width = max_x - min_x
     height = max_y - min_y
     depth = max_z - min_z
-    
+
     # Calculate scale factors to make the object a unit cube
     scale_x = 1.0 / width if width != 0 else 1.0
     scale_y = 1.0 / height if height != 0 else 1.0
     scale_z = 1.0 / depth if depth != 0 else 1.0
-    
+
     scale_factor = min(scale_x, scale_y, scale_z)
-    
+
     # Apply scaling to the object
     obj.scale *= scale_factor
     bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
     obj.location = (0, 0, 0)
-    
+
 
 def move_object_to_center(obj):
     bbox_corners = [obj.matrix_world @ Vector(corner) for corner in obj.bound_box]
